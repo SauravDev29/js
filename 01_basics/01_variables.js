fd9c990a6e38d41d96f3470e687b54160a4e17c8
SauravDev29 <developer.sauravkumar@gmail.com>
2025-06-28 01:29:50 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const a =10 ; // constant value / immutable
+let b = 15   // scope
+var c = 234 // mutable and not preferred 
+
+console.log();
+console.table([ a,b,c])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+
+// 1995 - Brandon eich - net scape communication  
+// MOkA was first name then LIVE Script and then javascript for popularity as JAVA was
+// at that time
+// NODE BUN DENO are runtime environment that run on v8 engine 
+ 
+/// EKMA international manages it , TC39 manages it.  members are goggle , microsoft , mozilla , microsoft
+// node is open source ,    V8 engine was fastest at that time and was nonblocking event 
+
+
+// alert(3+3) // we are using node js not browser
+
+let name = "saurav"
+let age = 21
+let isLoggedIn = false
+
+// number -> 2 to power 53
+//bigint 
+// string -> ""
+// boolean -> true/false
+//null  -> a standalone value
+// undefined ->
+// symbol -> unique
+
+// object 
+
+console.log(typeof " saurav") // string 
+console.log(typeof null); // object
